d625176cbff4ab4b0c96c9dbd8233371c4632d2d
trzl47 <brownty47@gmail.com>
2017-03-18 19:21:54 -0500

    Updated app entry point

---
+++ index.js
@@ -0,0 +1,11 @@
+// Libs
+import React from 'react';
+import { render } from 'react-dom';
+
+// Components
+import Scoreboard from './containers/Scoreboard';
+
+// CSS
+import './app.css';
+
+render(<Scoreboard />, document.getElementById('container'));
